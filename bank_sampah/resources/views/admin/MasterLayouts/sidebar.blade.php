b39cd0cb51e1ce48bdfecb0447c19a002a837361
Hanif Rimamtomo <65529407+Shortfire13@users.noreply.github.com>
2021-07-15 21:58:17 +0700

    sidebar

--- bank_sampah/resources/views/admin/MasterLayouts/sidebar.blade.php
+++ bank_sampah/resources/views/admin/MasterLayouts/sidebar.blade.php
@@ -2,69 +2,88 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="backend/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">CallTrash</span>
+        <img src="backend/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
+        <span class="brand-text font-weight-light">CallTrash</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="backend/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="backend/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Hanif Rimamtomo</a>
+            </div>
         </div>
-        <div class="info">
-          <a href="#" class="d-block">Hanif Rimamtomo</a>
-        </div>
-      </div>
 
-      <!-- SidebarSearch Form -->
-     
+        <!-- SidebarSearch Form -->
 
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-              </p>
-            </a>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="fas fa-fw fa-folder"></i>
-                  <span>Manajemen Users</span>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-                  <a class="dropdown-item" href="">Manajemen Admin</a>
-                  <a class="dropdown-item" href="">Manajemen Pegawai</a>
-                  <a class="dropdown-item" href="">Manajemen User</a>
-              </div>
-            </li>
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <i class="fas fa-cash-register"></i>
-              <p>Transaksi</p>
-            </a>
-          </li>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
 
-  @yield('content')
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Sidebar Administrator -->
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="fas fa-tachometer-alt nav-icon"></i>
+                        <p>Dashboard</p>
+                        <span class="right badge badge-danger"></span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-person-lines-fill align-middle"></i>
+                        <p>
+                            Manajemen
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Management Admin</p>
+                            </a>
+                        </li>
+                        <li class="nav-item menu-open">
+                            <a href="" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Manajemen User</p>
+                            </a>
+                        </li>
+                        <li class="nav-item menu-open">
+                            <a href="" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Manajemen Pegawai</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="fas fa-cash-register nav-icon"></i>
+                        <p>Transaksi</p>
+                        <span class="right badge badge-danger"></span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar -->
+</aside>
 
-    <!-- Main content -->
-    
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+@yield('content')
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
+<!-- Main content -->
+
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
+</aside>
+<!-- /.control-sidebar -->
