24b668bd59229fd43a49d3ed38161f8d979f8d32
geofannyher <geofannyher@gmail.com>
2021-07-18 08:14:44 -0400

    Api belum kelar

---
+++ bank_sampah/app/Http/Controllers/ProdukApiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdukApiController extends Controller
+{
+    public function produk()
+    {
+        return 'test';
+    }
+}
