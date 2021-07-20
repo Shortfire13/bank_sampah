7d195a85e667c0b0d04ecf9f865539b5575f74d5
Hanif <hanifsatrio12@gmail.com>
2021-07-20 09:37:56 +0700

    layout profile

--- bank_sampah/resources/views/user/profile.blade.php
+++ bank_sampah/resources/views/user/profile.blade.php
@@ -2,49 +2,6 @@
 
 @section('content')
   <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top">
-    <div class="container d-flex">
-
-      <div class="logo mr-auto">
-        <h1 class="text-light"><a href="index.html">Serenity</a></h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-      </div>
-
-      <nav class="nav-menu d-none d-lg-block">
-        <ul>
-          <li><a href="index.html">Home</a></li>
-
-          <li class="drop-down"><a href="#">About</a>
-            <ul>
-              <li><a href="about.html">About Us</a></li>
-              <li class="active"><a href="team.html">Team</a></li>
-
-              <li class="drop-down"><a href="#">Drop Down 2</a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-
-          <li><a href="pricing.html">Pricing</a></li>
-          <li><a href="services.html">Services</a></li>
-          <li><a href="portfolio.html">Portfolio</a></li>
-          <li><a href="blog.html">Blog</a></li>
-          <li><a href="contact.html">Contact</a></li>
-
-          <li class="get-started"><a href="#get-started">Get Started</a></li>
-        </ul>
-      </nav><!-- .nav-menu -->
-
-    </div>
-  </header><!-- End Header -->
-
   <main id="main">
 
     <!-- ======= Breadcrumbs ======= -->
@@ -64,31 +21,49 @@
     <!-- ======= Team Section ======= -->
     <section id="team" class="team">
       <div class="container">
-
-        <div class="row">
-
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up">
-              <div class="member-img">
-                <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                <div class="social">
-                  <a href=""><i class="icofont-twitter"></i></a>
-                  <a href=""><i class="icofont-facebook"></i></a>
-                  <a href=""><i class="icofont-instagram"></i></a>
-                  <a href=""><i class="icofont-linkedin"></i></a>
-                </div>
+        <div class="card">
+            <div class="card-header bg-success text-light">
+            <h3 class="text-center">Perbarui Data Anda</h3>
+          </div>
+          <div class="card-body">
+            <form>
+              <div class="form-group">
+                <label for="InputNama">Nama Lengkap</label>
+                <input type="text" class="form-control" id="nama" placeholder="Masukkan Nama Lengkap Anda">
               </div>
-              <div class="member-info">
-                <h4>Walter White</h4>
-                <span>Chief Executive Officer</span>
-                <p>Animi est delectus alias quam repellendus nihil nobis dolor. Est sapiente occaecati et dolore. Omnis aut ut nesciunt explicabo qui. Eius nam deleniti ut omnis repudiandae perferendis qui. Neque non quidem sit sed pariatur quia modi ea occaecati. Incidunt ea non est corporis in.</p>
+              <div class="form-group">
+                <label for="InputUsername">Username</label>
+                <input type="text" class="form-control" id="username" placeholder="Masukkan Nama Lengkap Anda">
               </div>
-            </div>
-          </div>     
-
+              <div class="form-group">
+                <label for="InputEmail">Email</label>
+                <input type="text" class="form-control" id="email" placeholder="Masukkan Email Anda">
+              </div>
+              <div class="form-group">
+                <label for="InputAlamat">Alamat</label>
+                <input type="text" class="form-control" id="alamat" placeholder="Masukkan Alamat Rumah Anda">
+              </div>
+              <div class="form-group">
+                <label for="InputFoto">Foto Profile</label>
+                <input type="file" class="form-control mb-5" id="alamat" placeholder="Masukkan Foto Profile Anda">
+              </div>
+              <div class="form-group">
+                <label for="InputPasswordLama">Password Lama</label>
+                <input type="password" class="form-control" id="passwordlama" placeholder="Masukkan Password Lama Anda">
+              </div>
+              <div class="form-group">
+                <label for="InputPasswordBaru">Password Baru</label>
+                <input type="password" class="form-control" id="passwordbaru" placeholder="Masukkan Password Baru Anda">
+              </div>
+              <div class="form-group">
+                <label for="KonfirmasiPassword">Password Baru</label>
+                <input type="password" class="form-control" id="confirmpass" placeholder="Masukkan Lagi Password Baru Anda">
+              </div>
+              <a href="" class="btn btn-success col-md-12 mt-2">Perbarui</a>
+            </form>
+          </div>
         </div>
 
-      </div>
     </section><!-- End Team Section -->
 
   </main><!-- End #main -->
