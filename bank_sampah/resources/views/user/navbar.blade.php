d23d3d0970742003a05989b8e0b84991628be9f7
faiziqbald <faiziqbald@gmail.com>
2021-07-20 21:00:48 +0700

    Minor Update Auth

--- bank_sampah/resources/views/user/navbar.blade.php
+++ bank_sampah/resources/views/user/navbar.blade.php
@@ -1,4 +1,4 @@
-  <!-- ======= Header ======= -->
+-----------------------------------  <!-- ======= Header ======= -->
   <header id="header" class="fixed-top">
     <div class="container d-flex">
 
@@ -12,8 +12,44 @@
         <ul>
             <li class=""><a href="{{route('riwayat')}}">Riwayat</a></li>
           <li class=""><a href="{{route('index')}}">Home</a></li>
-          <li><a href="{{route('register')}}">Register</a></li>
-          <li class="get-started"><a href="{{route('login')}}">Login</a></li>
+
+          @guest
+          @if (Route::has('register'))
+          <li class="nav-item">
+              <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+          </li>
+          @endif
+
+          @if (Route::has('login'))
+              <li class="get-started">
+                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+              </li>
+          @endif
+
+         
+      @else
+          <li class="nav-item dropdown">
+              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                  {{ Auth::user()->name }}
+              </a>
+
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('profile') }}">
+                  Profile
+              </a>
+                
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                     onclick="event.preventDefault();
+                                   document.getElementById('logout-form').submit();">
+                      {{ __('Logout') }}
+                  </a>
+
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                      @csrf
+                  </form>
+              </div>
+          </li>
+      @endguest
         </ul>
       </nav><!-- .nav-menu -->
 
