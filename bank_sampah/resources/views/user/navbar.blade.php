24cf5709f8345be427b9eef6c3636e1ae8f9c25e
Hanif <hanifsatrio12@gmail.com>
2021-07-25 20:43:30 +0700

    dipercantik doang

--- bank_sampah/resources/views/user/navbar.blade.php
+++ bank_sampah/resources/views/user/navbar.blade.php
@@ -1,60 +1,61 @@
------------------------------------  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top">
+-----------------------------------
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top">
     <div class="container d-flex">
 
-      <div class="logo mr-auto">
-        <h1 class="text-light"><a href="{{route('index')}}">CallTrash</a></h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-      </div>
+        <div class="logo mr-auto">
+            <h1 class="text-light"><a href="{{route('index')}}">CallTrash</a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+        </div>
 
-      <nav class="nav-menu d-none d-lg-block">
-        <ul>
-            
-          <li class=""><a href="{{route('index')}}">Home</a></li>
+        <nav class="nav-menu d-none d-lg-block">
+            <ul>
 
-          @guest
+                <li class=""><a href="{{route('index')}}">Home</a></li>
 
-          @if (Route::has('register'))
-          <li class="nav-item">
-              <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-          </li>
-          @endif
+                @guest
 
-          @if (Route::has('login'))
-              <li class="get-started">
-                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-              </li>
-          @endif
+                @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+                @endif
 
-         
-      @else
-           <li class=""><a href="{{route('riwayat')}}">Riwayat</a></li>
+                @if (Route::has('login'))
+                <li class="get-started">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @endif
 
-          <li class="nav-item dropdown">
-              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                  {{ Auth::user()->nama_user }}
-              </a>
 
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('profile') }}">
-                  Profile
-              </a>
-                
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                     onclick="event.preventDefault();
+                @else
+                <li class=""><a href="{{route('riwayat')}}">Riwayat</a></li>
+
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->nama_user }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('profile') }}">
+                            Profile
+                        </a>
+
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                    document.getElementById('logout-form').submit();">
-                      {{ __('Logout') }}
-                  </a>
+                            {{ __('Logout') }}
+                        </a>
 
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                      @csrf
-                  </form>
-              </div>
-          </li>
-      @endguest
-        </ul>
-      </nav><!-- .nav-menu -->
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+                @endguest
+            </ul>
+        </nav><!-- .nav-menu -->
 
     </div>
-  </header><!-- End Header -->
+</header><!-- End Header -->
